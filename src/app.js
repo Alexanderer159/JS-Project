6b373225c28268d52b3245fc7790d2537505bd40
Alexanderer159 <aledeyavorsky@gmail.com>
2025-02-11 16:44:11 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+const action = ['ate', 'peed', 'crushed', 'broke'];
+const what = ['my homework', 'my phone', 'the car'];
+const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+function Randomizer(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+const randomExcuse = `${Randomizer(who)} ${Randomizer(action)} ${Randomizer(what)} ${Randomizer(when)}`
+
+console.log(randomExcuse)
+}
+const LolExcuse = document.getElementById("LolExcuse"); //Gonna be honest, i had to look online how to get a command line into HTML front
+const originalConsoleLog = console.log;
+console.log = function(message) {
+    originalConsoleLog(message);
+    LolExcuse.innerHTML += message + "<br>";
 };
