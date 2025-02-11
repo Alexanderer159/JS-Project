7e649fba8018ff8a1b299aec6081ff65f9b8ae79
Alexanderer159 <aledeyavorsky@gmail.com>
2025-02-11 17:23:50 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -16,11 +16,6 @@ function Randomizer(arr) {
 }
 const randomExcuse = `${Randomizer(who)} ${Randomizer(action)} ${Randomizer(what)} ${Randomizer(when)}`
 
-console.log(randomExcuse)
+document.getElementById("LolExcuse").innerHTML = randomExcuse; //Gonna be honest, i had to look online how to get a command line into HTML front
+;
 }
-const LolExcuse = document.getElementById("LolExcuse"); //Gonna be honest, i had to look online how to get a command line into HTML front
-const originalConsoleLog = console.log;
-console.log = function(message) {
-    originalConsoleLog(message);
-    LolExcuse.innerHTML += message + "<br>";
-};
